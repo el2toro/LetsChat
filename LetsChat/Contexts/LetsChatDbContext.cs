c790c4be4f5cc81a294b79f27b14d95ecb4b803e
el2toro <vasea.badragan@gmail.com>
2025-04-02 13:28:19 +0200

    Add db context

---
+++ LetsChat/Contexts/LetsChatDbContext.cs
@@ -0,0 +1,43 @@
+﻿using LetsChat.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LetsChat.Data;
+
+public class LetsChatDbContext(IConfiguration configuration) : DbContext
+{
+    private readonly IConfiguration _configuration = configuration;
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(_configuration.GetConnectionString("DbConnection"));
+    }
+
+    public DbSet<User> Users { get; set; }
+    public DbSet<Message> Messages { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<User>().HasKey(x => x.Id);
+
+        modelBuilder.Entity<Message>().HasKey(x => x.Id);
+        modelBuilder.Entity<Message>()
+            .HasOne(m => m.Sender)
+            .WithOne()
+            .OnDelete(DeleteBehavior.NoAction);
+
+        modelBuilder.Entity<Message>()
+            .HasOne(m => m.Receiver)
+            .WithOne()
+            .OnDelete(DeleteBehavior.NoAction);
+
+        modelBuilder.Entity<Message>()
+            .HasIndex(m => m.SenderId)
+            .IsUnique(false);
+
+        modelBuilder.Entity<Message>()
+            .HasIndex(m => m.ReceiverId)
+            .IsUnique(false);
+    }
+}
