f8cc5d8393e58db117a385a26b4ce03a7088cc72
el2toro <vasea.badragan@gmail.com>
2025-04-02 13:27:47 +0200

    Add repositories

---
+++ LetsChat/Repositories/MessageRepository.cs
@@ -0,0 +1,39 @@
+﻿namespace LetsChat.Repositories;
+
+public interface IMessageRepository
+{
+    Task<IEnumerable<Message>> GetMessages(int senderId, int receiverId);
+    Task SendMessage(MessageDto message, CancellationToken cancellationToken);
+}
+public class MessageRepository(LetsChatDbContext dbContext) : IMessageRepository
+{
+    public async Task<IEnumerable<Message>> GetMessages(int senderId, int receiverId)
+    {
+        var messages = await dbContext.Messages
+            .Where(m => m.SenderId == senderId && m.ReceiverId == receiverId)
+            .ToListAsync();
+
+        return messages;
+    }
+
+    public async Task SendMessage(MessageDto message, CancellationToken cancellationToken)
+    {
+        var createdMessage = MapMessage(message);
+
+        dbContext.Messages.Add(createdMessage);
+        await dbContext.SaveChangesAsync(cancellationToken);
+    }
+
+    private Message MapMessage(MessageDto messageDto)
+    {
+        return new Message
+        {
+            SenderId = messageDto.SenderId,
+            ReceiverId = messageDto.ReceiverId,
+            Content = messageDto.Content,
+            SendAt = DateTime.Now,
+            IsDeleted = messageDto.IsDeleted,
+            IsRead = messageDto.IsRead
+        };
+    }
+}
---
+++ LetsChat/Repositories/UserRepository.cs
@@ -0,0 +1,40 @@
+﻿namespace LetsChat.Repositories;
+
+public interface IUserRepository
+{
+    Task<IEnumerable<User>> GetUsers(CancellationToken cancellationToken);
+    Task<User> GetUserById(int id, CancellationToken cancellationToken);
+    Task<IEnumerable<Message>> GetUserMessagesById(int senderId, int receiverId, CancellationToken cancellationToken);
+    Task CreateUser(User user, CancellationToken cancellationToken);
+    Task<User> UpdateUser(User user, CancellationToken cancellationToken);
+}
+public class UserRepository(LetsChatDbContext dbContext) : IUserRepository
+{
+    public async Task CreateUser(User user, CancellationToken cancellationToken)
+    {
+        var user = await dbContext.Users.FindAsync(1);
+        await dbContext.Users.AddAsync(user,
+    }
+
+    public async Task<User> GetUserById(int id, CancellationToken cancellationToken)
+    {
+        return await dbContext.Users.FirstAsync(u => u.Id == id, cancellationToken);
+    }
+
+    public async Task<IEnumerable<Message>> GetUserMessagesById(int senderId, int receiverId, CancellationToken cancellationToken)
+    {
+        return await dbContext.Messages
+            .Where(m => m.SenderId == senderId && m.ReceiverId == receiverId && !m.IsDeleted)
+            .ToListAsync(cancellationToken);
+    }
+
+    public async Task<IEnumerable<User>> GetUsers(CancellationToken cancellationToken)
+    {
+        return await dbContext.Users.ToListAsync(cancellationToken);
+    }
+
+    public Task<User> UpdateUser(User user, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
