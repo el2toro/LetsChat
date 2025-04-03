e94d3bcee7051cdfe09a31b755865297a857c9e1
el2toro <vasea.badragan@gmail.com>
2025-04-03 21:17:41 +0200

    Add authentication repository

---
+++ LetsChat/Repositories/AuthenticationRepository.cs
@@ -0,0 +1,24 @@
+﻿using LetsChat.Authentication.Dtos;
+
+namespace LetsChat.Repositories;
+
+public interface IAuthenticationRepository
+{
+    Task<User> Login(LoginDto loginDto);
+    Task Logout();
+}
+public class AuthenticationRepository(LetsChatDbContext dbContext) : IAuthenticationRepository
+{
+    public async Task<User> Login(LoginDto loginDto)
+    {
+        var user = await dbContext.Users
+            .FirstOrDefaultAsync(u => u.Username == loginDto.UserName && u.Password == loginDto.Password);
+
+        return user;
+    }
+
+    public Task Logout()
+    {
+        throw new NotImplementedException();
+    }
+}
