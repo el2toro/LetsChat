ff09884fb529f25a4d433f2ea4cb6315455e8028
el2toro <vasea.badragan@gmail.com>
2025-04-04 16:35:02 +0200

    Add GetLastMessage entdpoint  and handler

---
+++ LetsChat/Messages/GetLastMessage/GetLastMessageEndpoint.cs
@@ -0,0 +1,14 @@
+﻿namespace LetsChat.Messages.GetLastMessage;
+
+public class GetLastMessageEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/message", async ([FromQuery] int senderId, int receiverId, ISender sender) =>
+        {
+            var request = await sender.Send(new GetLastMessageRequest(senderId, receiverId));
+            return Results.Ok(request.Message);
+        })
+        .WithName("GetLastMessage");
+    }
+}
---
+++ LetsChat/Messages/GetLastMessage/GetLastMessageHandler.cs
@@ -0,0 +1,27 @@
+﻿namespace LetsChat.Messages.GetLastMessage;
+
+public record GetLastMessageRequest(int SenderId, int ReceiverId) : IRequest<GetLastMessageResult>;
+public record GetLastMessageResult(MessageDto Message);
+public class GetLastMessageHandler(IMessageRepository messageRepository)
+    : IRequestHandler<GetLastMessageRequest, GetLastMessageResult>
+{
+    public async Task<GetLastMessageResult> Handle(GetLastMessageRequest request, CancellationToken cancellationToken)
+    {
+        var message = await messageRepository.GetLastMessage(request.SenderId, request.ReceiverId, cancellationToken);
+        return new GetLastMessageResult(MapToDto(message));
+    }
+
+    private MessageDto MapToDto(Message message)
+    {
+        return new MessageDto
+        {
+            SenderId = message.SenderId,
+            ReceiverId = message.ReceiverId,
+            Content = message.Content,
+            Id = message.Id,
+            IsDeleted = message.IsDeleted,
+            IsRead = message.IsRead,
+            SendAt = message.SendAt.ToString()
+        };
+    }
+}
