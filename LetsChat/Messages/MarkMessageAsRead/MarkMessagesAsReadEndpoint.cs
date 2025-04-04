a059054b5ac9b707f4489d1e206b43ad5c5a1b05
el2toro <vasea.badragan@gmail.com>
2025-04-04 16:35:14 +0200

    Manage read messages

---
+++ LetsChat/Messages/MarkMessageAsRead/MarkMessagesAsReadEndpoint.cs
@@ -0,0 +1,16 @@
+﻿namespace LetsChat.Messages.MarkMessageAsRead;
+
+public record MarkMessagesAsReadRequest(int SenderId, int ReceiverId);
+
+public class MarkMessagesAsReadEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPost("/messages", async ([FromBody] MarkMessagesAsReadRequest request, ISender sender) =>
+        {
+            await sender.Send(new MarkMessagesAsReadQuery(request.SenderId, request.ReceiverId));
+            return Results.Ok();
+        })
+        .WithName("MarkMessagesAsRead");
+    }
+}
---
+++ LetsChat/Messages/MarkMessageAsRead/MarkMessagesAsReadHandler.cs
@@ -0,0 +1,13 @@
+﻿namespace LetsChat.Messages.MarkMessageAsRead;
+
+public record MarkMessagesAsReadQuery(int SenderId, int ReceiverId) : IRequest<MarkMessagesAsReadResult>;
+public record MarkMessagesAsReadResult(bool IsSuccess);
+public class MarkMessagesAsReadHandler(IMessageRepository messageRepository)
+    : IRequestHandler<MarkMessagesAsReadQuery, MarkMessagesAsReadResult>
+{
+    public async Task<MarkMessagesAsReadResult> Handle(MarkMessagesAsReadQuery request, CancellationToken cancellationToken)
+    {
+        await messageRepository.MarkMessagesAsRead(request.SenderId, request.ReceiverId, cancellationToken);
+        return new MarkMessagesAsReadResult(true);
+    }
+}
