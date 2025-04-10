03e74dee8e83deafeec1dbe18f4aca5e45297c0f
el2toro <vasea.badragan@gmail.com>
2025-04-10 18:06:43 +0200

    Implement DeleteUser handler and endpoint

---
+++ LetsChat/Users/DeleteUser/DeleteUserEndpoint.cs
@@ -0,0 +1,19 @@
+﻿using LetsChat.Users.GetUsers;
+
+namespace LetsChat.Users.DeleteUser;
+
+public class DeleteUserEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapDelete("/user/{userId}", async (int userId, ISender sender) =>
+        {
+            await sender.Send(new DeleteUserRequest(userId));
+            return Results.NoContent();
+        })
+        .WithDisplayName("DeleteUser")
+        .Produces<GetUsersResult>(StatusCodes.Status200OK)
+        .WithDescription("Delete User")
+        .WithSummary("Delete User");
+    }
+}
---
+++ LetsChat/Users/DeleteUser/DeleteUserHandler.cs
@@ -0,0 +1,13 @@
+﻿namespace LetsChat.Users.DeleteUser;
+
+public record DeleteUserRequest(int UserId) : IRequest<DeleteUserResult>;
+public record DeleteUserResult(bool IsSuccess);
+public class DeleteUserHandler(IUserRepository userRepository)
+    : IRequestHandler<DeleteUserRequest, DeleteUserResult>
+{
+    public async Task<DeleteUserResult> Handle(DeleteUserRequest request, CancellationToken cancellationToken)
+    {
+        await userRepository.DeleteUser(request.UserId, cancellationToken);
+        return new DeleteUserResult(true);
+    }
+}
