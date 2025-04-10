0329cf8ff28875b43607143d80419cb573bee0f6
el2toro <vasea.badragan@gmail.com>
2025-04-10 18:06:58 +0200

    Implement GetUserById handler and endpoint

---
+++ LetsChat/Users/GetUserById/GetUserByIdEndpoint.cs
@@ -0,0 +1,17 @@
+﻿namespace LetsChat.Users.GetUserById;
+
+public class GetUserByIdEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/user/{userId}", async (int userId, ISender sender) =>
+        {
+            var result = await sender.Send(new GetUserByIdRequest(userId));
+            return Results.Ok(result);
+        })
+        .WithDisplayName("GetUserById")
+        .Produces(StatusCodes.Status200OK)
+        .WithDescription("Get User By Id")
+        .WithSummary("Get User By Id");
+    }
+}
---
+++ LetsChat/Users/GetUserById/GetUserByIdHandler.cs
@@ -0,0 +1,30 @@
+﻿namespace LetsChat.Users.GetUserById;
+
+public record GetUserByIdRequest(int UserId) : IRequest<GetUserByIdResult>;
+public record GetUserByIdResult(UserDto UserDto);
+
+public class GetUserByIdHandler(IUserRepository userRepository)
+    : IRequestHandler<GetUserByIdRequest, GetUserByIdResult>
+{
+    public async Task<GetUserByIdResult> Handle(GetUserByIdRequest request, CancellationToken cancellationToken)
+    {
+        var user = await userRepository.GetUserById(request.UserId, cancellationToken);
+        return new GetUserByIdResult(MapToDto(user));
+
+    }
+
+    //TODO: use automapper
+    private UserDto MapToDto(User user)
+    {
+        return new UserDto
+        {
+            Id = user.Id,
+            Name = user.Name,
+            Email = user.Email,
+            FullName = string.Concat(user.Name, " ", user.Surname),
+            Surename = user.Surname,
+            Username = user.Username,
+            Password = user.Password
+        };
+    }
+}
