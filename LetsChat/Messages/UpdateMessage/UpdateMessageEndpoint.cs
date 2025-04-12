6769056d9762dde5d80309a681a002ce5c05ed63
el2toro <vasea.badragan@gmail.com>
2025-04-12 17:07:47 +0200

    Add UpdateMessage endpoint and handler implementation

--- LetsChat/Messages/UpdateMessage/UpdateMessageEndpoint.cs
+++ LetsChat/Messages/UpdateMessage/UpdateMessageEndpoint.cs
@@ -4,6 +4,11 @@ public class UpdateMessageEndpoint : ICarterModule
 {
     public void AddRoutes(IEndpointRouteBuilder app)
     {
-        //throw new NotImplementedException();
+        app.MapPut("/message", async ([FromBody] MessageDto messageDto, ISender sender) =>
+        {
+            var result = await sender.Send(new UpdateMessageRequest(messageDto));
+            return Results.Ok(result.MessageDto);
+        })
+        .WithDisplayName("UpdateMessage");
     }
 }
--- LetsChat/Messages/UpdateMessage/UpdateMessageHandler.cs
+++ LetsChat/Messages/UpdateMessage/UpdateMessageHandler.cs
@@ -1,5 +1,23 @@
 ﻿namespace LetsChat.Messages.UpdateMessage;
 
-public class UpdateMessageHandler
+public record UpdateMessageRequest(MessageDto MessageDto) : IRequest<UpdateMessageResult>;
+public record UpdateMessageResult(MessageDto MessageDto);
+public class UpdateMessageHandler(IMessageRepository messageRepository)
+    : IRequestHandler<UpdateMessageRequest, UpdateMessageResult>
 {
+    public async Task<UpdateMessageResult> Handle(UpdateMessageRequest request, CancellationToken cancellationToken)
+    {
+        var message = new Message
+        {
+            Id = request.MessageDto.Id,
+            Content = request.MessageDto.Content
+        };
+
+        var result = await messageRepository.UpdateMessage(message, cancellationToken);
+
+        //TODO: add proper mapping
+        request.MessageDto.Content = request.MessageDto.Content;
+
+        return new UpdateMessageResult(request.MessageDto);
+    }
 }
