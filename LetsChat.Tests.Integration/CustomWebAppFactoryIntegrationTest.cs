e8d1740abab48ba803976562b8da22c8c6fee4e3
el2toro <vasea.badragan@gmail.com>
2025-04-13 00:24:47 +0200

    Configure in memory database for integration tests

---
+++ LetsChat.Tests.Integration/CustomWebAppFactoryIntegrationTest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using LetsChat.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace LetsChat.Tests.Integration;
+
+public class CustomWebAppFactoryIntegrationTest : WebApplicationFactory<Program>
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        // Modify services specifically for testing (e.g., in-memory database)
+        builder.ConfigureServices(services =>
+        {
+            var options = new DbContextOptionsBuilder<LetsChatDbContext>()
+                .UseInMemoryDatabase("IntegrationTestDb")
+                .Options;
+
+            services.AddSingleton(options);
+            services.AddDbContext<LetsChatDbContext>(opt => opt.UseInMemoryDatabase("IntegrationTestDb"));
+        });
+    }
+}
