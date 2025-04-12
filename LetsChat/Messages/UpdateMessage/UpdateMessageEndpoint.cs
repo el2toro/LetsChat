1679d8ad0669090e4cf6907380e058102be1e3f6
el2toro <vasea.badragan@gmail.com>
2025-04-13 00:27:18 +0200

    Bug fix wrong endpoint verb

--- LetsChat/Messages/UpdateMessage/UpdateMessageEndpoint.cs
+++ LetsChat/Messages/UpdateMessage/UpdateMessageEndpoint.cs
@@ -4,7 +4,7 @@ public class UpdateMessageEndpoint : ICarterModule
 {
     public void AddRoutes(IEndpointRouteBuilder app)
     {
-        app.MapPut("/message", async ([FromBody] MessageDto messageDto, ISender sender) =>
+        app.MapPatch("/message", async ([FromBody] MessageDto messageDto, ISender sender) =>
         {
             var result = await sender.Send(new UpdateMessageRequest(messageDto));
             return Results.Ok(result.MessageDto);
