f865964ab6c0907afcfa8bfa384577d71b1f8d5d
el2toro <vasea.badragan@gmail.com>
2025-04-21 20:01:41 +0200

    Fix surename

--- LetsChat/Migrations/20250421174450_UpdateUserModel.Designer.cs
+++ LetsChat/Migrations/20250421174450_UpdateUserModel.Designer.cs
@@ -4,6 +4,7 @@ using LetsChat.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace LetsChat.Migrations
 {
     [DbContext(typeof(LetsChatDbContext))]
-    partial class LetsChatDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250421174450_UpdateUserModel")]
+    partial class UpdateUserModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ LetsChat/Migrations/20250421174450_UpdateUserModel.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LetsChat.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateUserModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Surname",
+                table: "Users",
+                newName: "Surename");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Surename",
+                table: "Users",
+                newName: "Surname");
+        }
+    }
+}
