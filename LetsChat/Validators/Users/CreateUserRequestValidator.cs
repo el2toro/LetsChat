03366960d1100a4309d00b4a38691f70877c0a7b
el2toro <vasea.badragan@gmail.com>
2025-04-21 19:14:00 +0200

    Add validation rules

---
+++ LetsChat/Validators/Users/CreateUserRequestValidator.cs
@@ -0,0 +1,38 @@
+﻿using LetsChat.Users.CreateUser;
+
+namespace LetsChat.Validators.Users;
+
+public class CreateUserRequestValidator : AbstractValidator<CreateUserRequest>
+{
+    public CreateUserRequestValidator()
+    {
+        string message = "is required, should have at least 4 characters";
+
+        RuleFor(x => x.UserDto)
+            .NotNull()
+            .WithMessage("UserDto object is required");
+
+        RuleFor(x => x.UserDto.Username)
+            .NotNull()
+            .MinimumLength(4)
+            .WithMessage($"Username {message}");
+
+        RuleFor(x => x.UserDto.Name)
+             .NotNull()
+             .MinimumLength(4)
+             .WithMessage($"Name {message}");
+
+        RuleFor(x => x.UserDto.Surename)
+            .NotNull()
+            .MinimumLength(4)
+            .WithMessage($"Surename {message}");
+
+        RuleFor(x => x.UserDto.Email)
+            .Matches(@"^[^@\s]+@[^@\s]+\.[^@\s]+$")
+            .WithMessage("Email is not valid");
+
+        RuleFor(x => x.UserDto.Password)
+            .Matches(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$")
+            .WithMessage("Password must contain at least 8 characters, one uppercase letter, one lowercase letter, and one number");
+    }
+}
---
+++ LetsChat/Validators/Users/GetUserByIdRequestValidator.cs
@@ -0,0 +1,11 @@
+﻿using LetsChat.Users.GetUserById;
+
+namespace LetsChat.Validators.Users;
+
+public class GetUserByIdRequestValidator : AbstractValidator<GetUserByIdRequest>
+{
+    public GetUserByIdRequestValidator()
+    {
+        RuleFor(x => x.UserId).GreaterThan(0).WithMessage("UserId should be greater than 0");
+    }
+}
