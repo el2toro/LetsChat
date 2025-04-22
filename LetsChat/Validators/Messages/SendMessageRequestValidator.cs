1ab3781e694b7a655af401a434322eafc8dd954d
el2toro <vasea.badragan@gmail.com>
2025-04-22 17:13:07 +0200

    Add validation rules for SendMessageRequest

---
+++ LetsChat/Validators/Messages/SendMessageRequestValidator.cs
@@ -0,0 +1,26 @@
+﻿using LetsChat.Messages.SendMessage;
+
+namespace LetsChat.Validators.Messages;
+
+public class SendMessageRequestValidator : AbstractValidator<SendMesageRequest>
+{
+    public SendMessageRequestValidator()
+    {
+        RuleFor(x => x.Message)
+            .NotNull()
+            .WithMessage("Message cannot be null");
+
+        RuleFor(x => x.Message.SenderId)
+            .GreaterThan(0)
+            .WithMessage("SenderId should be greater than 0");
+
+        RuleFor(x => x.Message.ReceiverId)
+            .GreaterThan(0)
+            .WithMessage("ReceiverId should be greater than 0");
+
+        RuleFor(x => x.Message.Content)
+            .NotNull()
+            .NotEmpty()
+            .WithMessage("Message Content cannot be null or empty space");
+    }
+}
