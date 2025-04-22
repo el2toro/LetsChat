311fec250004628fd55211b18db13010f95f8c0f
el2toro <vasea.badragan@gmail.com>
2025-04-22 17:34:02 +0200

    Add validation rules for UpdateMessageRequest

---
+++ LetsChat/Validators/Messages/UpdateMessageRequestValidator.cs
@@ -0,0 +1,26 @@
+﻿using LetsChat.Messages.UpdateMessage;
+
+namespace LetsChat.Validators.Messages;
+
+public class UpdateMessageRequestValidator : AbstractValidator<UpdateMessageRequest>
+{
+    public UpdateMessageRequestValidator()
+    {
+        RuleFor(x => x.MessageDto.Id)
+            .GreaterThan(0)
+            .WithMessage("Id should be greater than 0");
+
+        RuleFor(x => x.MessageDto.Content)
+            .NotNull()
+            .NotEmpty()
+            .WithMessage("Message Content cannot be null or empty space");
+
+        RuleFor(x => x.MessageDto.SenderId)
+            .GreaterThan(0)
+            .WithMessage("SenderId should be greater than 0");
+
+        RuleFor(x => x.MessageDto.ReceiverId)
+           .GreaterThan(0)
+           .WithMessage("ReceiverId should be greater than 0");
+    }
+}
