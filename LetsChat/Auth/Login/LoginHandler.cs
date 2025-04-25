726f40281c6c28993c63bc7f76bf761f7a07456c
el2toro <vasea.badragan@gmail.com>
2025-04-25 17:18:06 +0200

    Add login request validation rules

--- LetsChat/Auth/Login/LoginHandler.cs
+++ LetsChat/Auth/Login/LoginHandler.cs
@@ -6,11 +6,25 @@ namespace LetsChat.Auth.Login;
 public record LoginQuery(LoginDto LoginDto) : IRequest<LoginResult>;
 public record LoginResult(LoginDto LoginDto);
 
-public class LoginHandler(IAuthenticationRepository authenticationRepository, IJwtService jwtService)
+public class LoginQueryValidator : AbstractValidator<LoginQuery>
+{
+    public LoginQueryValidator()
+    {
+        RuleFor(x => x.LoginDto).NotNull().WithMessage("LoginDto object is required.");
+        RuleFor(x => x.LoginDto.UserName).NotEmpty().WithMessage("UserName is required.");
+        RuleFor(x => x.LoginDto.Password).NotEmpty().WithMessage("Password is required.");
+    }
+}
+
+public class LoginHandler(IAuthenticationRepository authenticationRepository,
+    IJwtService jwtService,
+    ILogger<LoginHandler> logger)
     : IRequestHandler<LoginQuery, LoginResult>
 {
     public async Task<LoginResult> Handle(LoginQuery request, CancellationToken cancellationToken)
     {
+        logger.LogInformation($"LoginHandler called with LoginDto: {request.LoginDto}");
+
         var user = await authenticationRepository.Login(request.LoginDto) ??
             throw new UserNotFoundException(request.LoginDto.UserId);
 
